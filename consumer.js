dbd69c0c7e3afac5a62224f9f9e7093fffd47381
Ahmad Naufal Hakim <ahmadnaufalhakim@gmail.com>
2020-06-01 14:17:51 +0700

    edit create student

--- consumer.js
+++ consumer.js
@@ -16,29 +16,35 @@ amqp.connect(AMQP_URL, (error, connection) => {
         ch = channel;
 
         ch.consume('create-student', (msg) => {
-            const obj = JSON.parse(msg.content);
-            pool.query(`INSERT INTO student(name) VALUES('${obj.payload}')`)
-                .then((results) => {
-                    console.log(results);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-            ch.ack(msg);
-        }, { noAck: false });
+            try {
+                const obj = JSON.parse(msg.content);
+                pool.query(`INSERT INTO student(name) VALUES('${obj.payload}')`)
+                    .then((results) => {
+                        console.log(results);
+                        ch.sendToQueue('create-student_res', Buffer.from(JSON.stringify(results)), { persistent: true });
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        ch.sendToQueue('create-student_res', Buffer.from(JSON.stringify(error)), { persistent: true });
+                    });
+            } catch (error) {
+                console.log(error);
+                ch.sendToQueue('create-student_res', Buffer.from(JSON.stringify(error), { persistent: true }));
+            }
+        }, { noAck: true });
 
         ch.consume('find-student-by-id', (msg) => {
             try {
                 const obj = JSON.parse(msg.content);
                 pool.query(`SELECT * FROM student WHERE registration_number = ${obj.payload}`)
-                .then((results) => {
-                    console.log(results[0]);
-                    ch.sendToQueue('find-student-by-id_res', Buffer.from(JSON.stringify(results[0])), { persistent: true });
-                })
-                .catch((error) => {
-                    console.log(error);
-                    ch.sendToQueue('find-student-by-id_res', Buffer.from(JSON.stringify(error)), { persistent: true });
-                });
+                    .then((results) => {
+                        console.log(results[0]);
+                        ch.sendToQueue('find-student-by-id_res', Buffer.from(JSON.stringify(results[0])), { persistent: true });
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        ch.sendToQueue('find-student-by-id_res', Buffer.from(JSON.stringify(error)), { persistent: true });
+                    });
             } catch (error) {
                 console.log(error);
                 ch.sendToQueue('find-student-by-id_res', Buffer.from(JSON.stringify(error)), { persistent: true });
